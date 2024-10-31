fe9ae7d3a7e881e60e2a8cdb4921d7996f33f062
Raje <rajevictory@gmail.com>
2024-10-31 10:43:16 -0700

    new changes made

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -39,19 +39,19 @@ export default function Contact() {
         <form ref={form} onSubmit={sendEmail} action="" className='w-[90%] md:w-[700px]'>
             <h1 className="text-center font-semibold text-2xl mb-5">Application <span className='text-orange-600'>Form</span></h1>
 
-            <input type="Name" name="user_name"  id="" placeholder=' Name' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="Email" name="user_email" id="" placeholder='Email' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="text" name="user_nextKin" id="" placeholder='Next Kin' className="w-full h-10 border rounded-md mb-3 px-2" />
-            <input type="text" name="user_lastName"  id="" placeholder='Last Name' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="text" name="user_otherName"  id="" placeholder='Other Name' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="text" name="user_phoneNo"  id="" placeholder='Phone No' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="text" name="user_country"  id="" placeholder='Country' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <input type="text" name="user_state"  id="" placeholder='State' className="w-full h-10 border rounded-md mb-3 px-2"  />
-            <textarea type="message" name="message" id="" placeholder='Message' className="w-full h-[150px] border rounded-md mb-3 px-2 resize-none" ></textarea>
+            <input type="Name" name="user_name"  id="" placeholder=' Name' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="Email" name="user_email" id="" placeholder='Email' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="text" name="user_nextKin" id="" placeholder='Next Kin' required className="w-full h-10 border rounded-md mb-3 px-2" />
+            <input type="text" name="user_lastName"  id="" placeholder='Last Name' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="text" name="user_otherName"  id="" placeholder='Other Name' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="text" name="user_phoneNo"  id="" placeholder='Phone No' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="text" name="user_country"  id="" placeholder='Country' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <input type="text" name="user_state"  id="" placeholder='State' required className="w-full h-10 border rounded-md mb-3 px-2"  />
+            <textarea type="message" name="message" id="" placeholder='Message' required className="w-full h-[150px] border rounded-md mb-3 px-2 resize-none" ></textarea>
            
            
             <div>
-              <input type="checkbox" name="agree"  id="" />
+              <input type="checkbox" name="agree"  id="" required />
               <label htmlFor="">Accept terms and conditions</label>
             </div>
             <button className='text-white bg-black block mx-auto mt-8 px-4 py-1 rounded-md hover:scale-105' >Submit</button>
--- src/pages/Log.jsx
+++ src/pages/Log.jsx
@@ -30,8 +30,8 @@ export default function Log() {
     <form ref={form} onSubmit={sendEmail} action="" className='w-[90%] md:w-[700px]'>
         <h1 className="text-center font-semibold text-2xl mb-5">Contact <span className='text-orange-600'>Me</span></h1>
 
-        <input type="Name" name="user_name" id="" placeholder='Name' className="w-full h-10 border rounded-md mb-3 px-2" />
-        <input type="Email" name="user_email" id="" placeholder='Email' className="w-full h-10 border rounded-md mb-3 px-2" />
+        <input type="Name" name="user_name" id="" placeholder='Name' required className="w-full h-10 border rounded-md mb-3 px-2" />
+        <input type="Email" name="user_email" id="" placeholder='Email' required className="w-full h-10 border rounded-md mb-3 px-2" />
         <textarea type="message" name="message" id="" placeholder='Message' className="w-full h-[150px] border rounded-md mb-3 px-2 resize-none" ></textarea>
         <button className='text-white bg-black block mx-auto mt-8 px-4 py-1 rounded-md hover:scale-105'>Submit</button>
     </form>
